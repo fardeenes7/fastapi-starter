a241e1dccd912c2b590b487688c4ba1dfdbc7289
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-03-16 16:16:47 +0600

    manage file

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
+    from api.index import app
     import uvicorn
-    uvicorn.run("api.index:app", host="0.0.0.0", port=8000, reload=True)
-    # uvicorn.run("api.index:app")
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
